d79349ebf9e19a7a20cc18db04ba53770bca6ce7
h437mori <108774538+h437mori@users.noreply.github.com>
2022-07-12 23:24:32 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とす\\
+る。
+{
+  int y;
+  if( x < 0 ){
+    exit(1);
+  }
+  y = sqrt(x);
+  return y;
+}
+
+int main(void)
+{
+  int i;
+  double *x = (double *)malloc(sizeof(double) * 100);
+
+  i = 0;
+    while( i <= 100 ){
+    x[i] = myRoot((double)i);
+    i++;
+  }
+
+  //以下は確認用(このままで良い)
+  printf("x[0]=%f\n", x[0]);
+  printf("x[1]=%f\n", x[1]);
+  printf("x[2]=%f\n", x[2]);
+  printf("x[3]=%f\n", x[3]);
+  printf("x[4]=%f\n", x[4]);
+    printf("x[99]=%f\n", x[99]);
+  printf("x[100]=%f\n", x[100]);
+
+  free(x);
+  return 0;
+}
+
+// 定義されていない関数があったため#include <stdlib.h>と#include <math.h>を追加\
+//した。
+// iに初期値が定義されていなかったためi=0を追加した。
+// x >= 0でないときにexit(1)とするためif文の条件はx < 0
